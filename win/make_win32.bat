81f63df3e4588d621fb4c8dd5c8770cbcf1fb42e
johnche <johnche@tencent.com>
2020-11-27 15:10:34 +0800

    win32编译

--- win/make_win32.bat
+++ win/make_win32.bat
@@ -1,5 +1,5 @@
 mkdir build & pushd build
-cmake -S ..\CMakeLists.win.txt -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 15 2017" ..
+cmake -S ..\CMakeLists.win.txt -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 16 2019" -A Win32 ..
 popd
 cmake --build build --config Release
 pause
