b3b259bd01f37c677d1123af3932df1d3ea8243b
johnche <johnche@tencent.com>
2020-11-27 16:11:05 +0800

    修正msys2下，CONFIG_VERSION的双引号被干掉的问题，并调整cmakelist的可读性

--- quickjs/quickjs.c
+++ quickjs/quickjs.c
@@ -6172,7 +6172,7 @@ void JS_DumpMemoryUsage(FILE *fp, const JSMemoryUsage *s, JSRuntime *rt)
 #ifdef CONFIG_BIGNUM
             "BigNum "
 #endif
-            "2020-11-08" " version, %d-bit, malloc limit: %"PRId64"\n\n",
+            CONFIG_VERSION " version, %d-bit, malloc limit: %"PRId64"\n\n",
             (int)sizeof(void *) * 8, (int64_t)(ssize_t)s->malloc_limit);
 #if 1
     if (rt) {
