0d2cd49aaeab19a51e5548dbebd001524caf0bb8
johnche <johnche@tencent.com>
2022-04-26 14:14:25 +0800

    新改法

--- src/v8-impl.cc
+++ src/v8-impl.cc
@@ -194,7 +194,6 @@ void Isolate::LowMemoryNotification() {
 
 Local<Value> Isolate::ThrowException(Local<Value> exception) {
     exception_ = exception->value_;
-    this->Escape(*exception);
     return Local<Value>(exception);
 }
 
@@ -249,12 +248,7 @@ Local<Message> v8::Exception::CreateMessage(Isolate* isolate_, Local<Value> exce
 
 void HandleScope::Escape_(JSValue* val) {
     if (JS_VALUE_HAS_REF_COUNT(*val)) {
-        if (escapes_.find(val) == escapes_.end()) {
-            escapes_.insert(val);
-        }
-        else {
-            JS_DupValueRT(isolate_->runtime_, *val);
-        }
+        JS_DupValueRT(isolate_->runtime_, *val);
     }
 }
 
@@ -264,12 +258,7 @@ void HandleScope::Exit() {
         //std::cout << prev_pos_ << "," << isolate_->value_alloc_pos_ << std::endl;
         isolate_->ForeachAllocValue(prev_pos_, isolate_->value_alloc_pos_, [this](JSValue* val, int idx){
             if (JS_VALUE_HAS_REF_COUNT(*val)) {
-                if (this->escapes_.find(val) == this->escapes_.end()) { //not excaped
-                    //std::cout << "free val type:" << JS_VALUE_GET_TAG(*val) << "," << val << ", idx:" << idx << std::endl;
-                    JS_FreeValueRT(isolate_->runtime_, *val);
-                //} else {
-                    //std::cout << "escaped val type:" << JS_VALUE_GET_TAG(*val) << "," << val << std::endl;
-                }
+                JS_FreeValueRT(isolate_->runtime_, *val);
             }
         });
         isolate_->value_alloc_pos_ = prev_pos_;
@@ -277,9 +266,7 @@ void HandleScope::Exit() {
     }
     
     if (JS_VALUE_HAS_REF_COUNT(scope_value_)) {
-        if (this->escapes_.find(&scope_value_) == this->escapes_.end()) { //not excaped
-            JS_FreeValueRT(isolate_->runtime_, scope_value_);
-        }
+        JS_FreeValueRT(isolate_->runtime_, scope_value_);
     }
 }
 
@@ -847,6 +834,7 @@ void ObjectTemplate::InitAccessors(Local<Context> context, JSValue obj) {
                 if (!JS_IsUndefined(isolate->exception_)) {
                     JSValue ex = isolate->exception_;
                     isolate->exception_ = JS_Undefined();
+                    isolate->Escape(&ex);
                     return JS_Throw(ctx, ex);
                 }
                 
@@ -876,6 +864,7 @@ void ObjectTemplate::InitAccessors(Local<Context> context, JSValue obj) {
                 if (!JS_IsUndefined(isolate->exception_)) {
                     JSValue ex = isolate->exception_;
                     isolate->exception_ = JS_Undefined();
+                    isolate->Escape(&ex);
                     return JS_Throw(ctx, ex);
                 }
                 
@@ -976,6 +965,7 @@ MaybeLocal<Function> FunctionTemplate::GetFunction(Local<Context> context) {
         if (!JS_IsUndefined(isolate->exception_)) {
             JSValue ex = isolate->exception_;
             isolate->exception_ = JS_Undefined();
+            isolate->Escape(&ex);
             return JS_Throw(ctx, ex);
         }
         
